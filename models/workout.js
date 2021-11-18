62ce102082480656d08a07a3d75a2a02e0958655
Victor Capella <capella89@outlook.com>
2021-11-17 22:36:23 -0500

    update duration

--- models/workout.js
+++ models/workout.js
@@ -1,50 +1,66 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const workoutExer = new Schema({
-  exercises: [
-    {
-      type: {
-        type: String,
-        trim: true,
-        required: "Type of exercise",
+const workoutExer = new Schema(
+  {
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Type of exercise",
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "Exercise name",
+        },
+        duration: {
+          type: Number,
+          required: "Exercise duration",
+        },
+        weight: {
+          type: Number,
+          trim: true,
+          required: "Equipment weight",
+        },
+        reps: {
+          type: Number,
+          trim: true,
+          required: "Number of reps",
+        },
+        sets: {
+          type: Number,
+          trim: true,
+          required: "Number of sets",
+        },
+        distance: {
+          type: Number,
+          trim: true,
+          required: "Total distance",
+        },
       },
-      name: {
-        type: String,
-        trim: true,
-        required: "Exercise name",
-      },
-      duration: {
-        type: Number,
-        required: "Exercise duration",
-      },
-      weight: {
-        type: Number,
-        trim: true,
-        required: "Equipment weight",
-      },
-      reps: {
-        type: Number,
-        trim: true,
-        required: "Number of reps",
-      },
-      sets: {
-        type: Number,
-        trim: true,
-        required: "Number of sets",
-      },
-      distance: {
-        type: Number,
-        trim: true,
-        required: "Total distance",
-      },
-    },
-  ],
+    ],
 
-  day: {
-    type: Date,
-    default: Date.now,
+    day: {
+      type: Date,
+      default: Date.now,
+    },
   },
+  {
+    toJSON: {
+      // virtuals: true,
+      getters: true,
+    },
+  }
+);
+
+//add duration
+
+workoutExer.virtual("totalDuration").get(function () {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
 });
 
 const Workout = mongoose.model("Workout", workoutExer);
